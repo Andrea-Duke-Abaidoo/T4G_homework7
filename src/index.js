c4d809d863ca5d504819d80ba56d6a00dd38d314
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-12-09 08:57:46 +0000

    corrections made

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-let username = prompt("What's your name?")
-let age = prompt("How old are you?")
-let email = prompt("What's your Email address?")
-if (age< 12){
-    alert(`Hi ${username}, you are ${age} years old and are to young to register. Sorry 😔😔 `)
-} else if (12 < age < 18){
-    alert(`Hi ${username}, you are ${age} years old and have limited options to register for. We will keep in touch via ${email} 👋🏽👋🏽 .`)
-} else { 
-    alert(`Hi ${username}, you are ${age} years old and can register for any option of your choosing. We will keep in touch via ${email} 👋🏽👋🏽  .`)
+let username = prompt("Enter your name?");
+let age = prompt("Enter your age");
+let email = prompt("Enter your Email address");
+if (age< "12 years old"){
+    alert(`Hi ${username}, you are ${age} years old and you are too young to register. Sorry 😔😔 `);
+} else if (12 < "age years old" < 18){
+    alert(`Hi ${username}, you are ${age} years old and you have limited options to register for. We will keep in touch via email: ${email} 👋🏽👋🏽 .`);
+} else if ("age years old" > 18){
+    alert(`Hi ${username}, you are ${age} years old and you can register for any option of your choosing. We will keep in touch via email: ${email} 👋🏽👋🏽  .`);
+}else {
+    alert("Please enter a valid age.");
 }
